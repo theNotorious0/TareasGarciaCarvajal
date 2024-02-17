7f362fd3e02dd8801a9d3ce0b7240ec99e0c4ecf
theNotorious0 <jpng251@estudiantec.cr>
2024-02-16 21:52:48 -0600

    Se abjunta el archivo de pruebas corregido por las sugerencias de Flake8

---
+++ Prueba_de_3_errores_en_flake_8.py
@@ -0,0 +1,8 @@
+def suma_numeros(a, b):
+    resultado = a + b
+    return resultado  # Error de sintaxis, debería ser "return" en lugar de "volver"
+
+numero1 = 10
+numero2 = 20
+
+print("La suma de", numero1, "y", numero2, "es:", suma_numeros(numero1, numero2))  # Error de estilo: línea demasiado larga
